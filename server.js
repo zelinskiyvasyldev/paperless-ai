6f00c8a07abe8317a05be5668523cb4a2ae450cb
Clusterzx <clusterz@protonmail.com>
2024-12-06 16:12:50 +0100

    minor fix

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   startScanning();
