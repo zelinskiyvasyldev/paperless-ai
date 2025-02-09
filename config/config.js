5bd19d94553e1cfe3a7688280afa0150f97ed4bd
Clusterzx <clusterz@protonmail.com>
2025-02-09 16:08:16 +0100

    Bump version

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ console.log('Loaded environment variables:', {
 });
 
 module.exports = {
-  PAPERLESS_AI_VERSION: '2.5.4',
+  PAPERLESS_AI_VERSION: '2.5.5',
   CONFIGURED: false,
   disableAutomaticProcessing: process.env.DISABLE_AUTOMATIC_PROCESSING || 'no',
   predefinedMode: process.env.PROCESS_PREDEFINED_DOCUMENTS,
